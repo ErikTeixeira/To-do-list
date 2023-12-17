a9db7bc0d892c12ddf4bd93c797364a53cddf0e3
ErikTeixeira <erik.barbosa515@gmail.com>
2023-12-17 15:59:25 -0300

    css arrumado

--- script/main.js
+++ script/main.js
@@ -1,14 +1,11 @@
-let inputNovaTarefa = document.querySelector("#inputNovaTarefa");
 
-let btnAddTarefa = document.querySelector("#btnAddTarefa");
-
-let listaTarefas = document.querySelector("#listaTarefas");
-
+let inputNovaTarefa = document.querySelectorAll(".inputNovaTarefa");
+let btnAddTarefa = document.querySelectorAll(".btnAddTarefa");
+let listaTarefas = document.querySelectorAll(".listaTarefas");
 
 let janelaEdicao = document.querySelector("#janelaEdicao");
 let janelaEdicaoFundo = document.querySelector("#janelaEdicaoFundo");
 let janelaEdicaoBtnFechar = document.querySelector("#janelaEdicaoBtnFechar");
-
 
 let btnAtualizarTarefa = document.querySelector("#btnAtualizarTarefa");
 let idTarefaEdicao = document.querySelector("#idTarefaEdicao");
@@ -16,26 +13,23 @@ let inputTarefaNomeEdicao = document.querySelector("#inputTarefaNomeEdicao");
 
 
 
-function adicionarTarefa(tarefa) {
+function adicionarTarefa(tarefa, lista, input) {
     let li = criarTagLi(tarefa);
-
-    listaTarefas.appendChild(li);
-
-    inputNovaTarefa.value = "";
+    lista.appendChild(li);
+    input.value = ''; // Limpar o valor do input após adicionar a tarefa
 }
 
 
-function gerarId(){
+function gerarId() {
     return Math.floor(Math.random() * 3000);
 }
 
 
 function criarTagLi(tarefa) {
-
     let li = document.createElement("li");
-    // vincular o elemento li ao id da tarefa
     li.id = tarefa.id;
 
+    // Criação de elementos para a tarefa
     let span = document.createElement("span");
     span.classList.add("textoTarefa");
     span.innerHTML = tarefa.nome;
@@ -45,13 +39,18 @@ function criarTagLi(tarefa) {
     let btnEditar = document.createElement("button");
     btnEditar.classList.add('btnAcao');
     btnEditar.innerHTML = `<i class="fa fa-edit"></i>`;
-    btnEditar.setAttribute('onclick', 'editar('+tarefa.id+')');
+
+    btnEditar.addEventListener('click', function() {
+        editar(tarefa.id);
+    });
 
     let btnExcluir = document.createElement("button");
     btnExcluir.classList.add('btnAcao');
     btnExcluir.innerHTML = `<i class="fa fa-trash"></i>`;
-    btnExcluir.setAttribute('onclick', 'excluir('+tarefa.id+')');
 
+    btnExcluir.addEventListener('click', function() {
+        excluir(tarefa.id);
+    });
 
     div.appendChild(btnEditar);
     div.appendChild(btnExcluir);
@@ -62,35 +61,44 @@ function criarTagLi(tarefa) {
 }
 
 
+
 function editar(idTarefa) {
     let li = document.getElementById(''+ idTarefa + '');
 
     if(li) {
+        // Colocar o titulo do retangulo de editar sendo o id
         idTarefaEdicao.innerHTML = '#' + idTarefa;
 
-        inputTarefaNomeEdicao.value = li.innerText;
+        inputTarefaNomeEdicao.value = li.querySelector('.textoTarefa').innerHTML;
+
         alternarJanelaEdicao();
-        
     } else {
         alert('Elemento HTML não encontrado!');
     }
 }
 
-function excluir( idTarefa ){
 
-    let confirmacao = window.confirm("Tem certeza que deseja excluir? ")
+
+function excluir(idTarefa) {
+    let confirmacao = window.confirm("Tem certeza que deseja excluir? ");
 
     if(confirmacao) {
         let li = document.getElementById(''+idTarefa+'');
         
         if(li) {
-            listaTarefas.removeChild(li);
+            li.parentNode.removeChild(li);
         }
     }
 }
 
 
+
 function alternarJanelaEdicao() {
+    // toggle('abrir') é um método da propriedade classList que alterna a presença da classe 'abrir'. Se a classe 'abrir' já estiver presente, ela será removida; se não estiver presente, será adicionada
+
+    // cada vez que alternarJanelaEdicao() é chamada, ela alterna a presença da classe 'abrir' nesses elementos, o que pode resultar em mostrar ou ocultar a janela de edição e o fundo correspondente
+
+    // "toggle" em inglês significa "alternar" ou "trocar"
     janelaEdicao.classList.toggle('abrir');
     janelaEdicaoFundo.classList.toggle('abrir');
 }
@@ -98,34 +106,36 @@ function alternarJanelaEdicao() {
 
 
 
-// https://www.toptal.com/developers/keycode    -  para saber o key code da tecla
+inputNovaTarefa.forEach(function(input, index) {
+    input.addEventListener("keypress", function(e) {
+        if( e.keyCode == 13) {
+            let tarefa = {
+                nome: input.value,
+                id: gerarId()
+            };
+            adicionarTarefa(tarefa, listaTarefas[index], input);
+        };
+    });
+});
 
-inputNovaTarefa.addEventListener("keypress", (e) => {
-    // 13 key code do enter
-    if( e.keyCode == 13) {
+
+
+btnAddTarefa.forEach(function(btn, index) {
+    btn.addEventListener("click", function(e) {
         let tarefa = {
-            nome: inputNovaTarefa.value,
+            nome: inputNovaTarefa[index].value,
             id: gerarId()
         };
-        adicionarTarefa(tarefa);
-    };
+        adicionarTarefa(tarefa, listaTarefas[index], inputNovaTarefa[index]);
+    });
 });
 
-
-
-
-btnAddTarefa.addEventListener("click", (e) => {
-    let tarefa = {
-        nome: inputNovaTarefa.value,
-        id: gerarId()
-    };
-    adicionarTarefa(tarefa);
-});
 
 
 janelaEdicaoBtnFechar.addEventListener("click", (e) => {
     alternarJanelaEdicao();
-})
+});
+
 
 
 btnAtualizarTarefa.addEventListener('click', (e) => {
@@ -142,9 +152,7 @@ btnAtualizarTarefa.addEventListener('click', (e) => {
 
     if(tarefaAtual) {
         let li = criarTagLi(tarefa);
-
-        // trocar a tarefa atual pela li nova
-        listaTarefas.replaceChild(li, tarefaAtual);
+        tarefaAtual.parentNode.replaceChild(li, tarefaAtual);
         alternarJanelaEdicao();
     } else {
         alert('Elemento HTML não encontrado!');
